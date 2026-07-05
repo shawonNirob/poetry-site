474cf0a514871a40fc2dae83c7e331b4dce54414
shawonNirob <shawonnirob16@gmail.com>
2026-07-06 02:32:27 +0600

    vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/poetry-site/',
+  base: '/',
   server: {
     host: true,
     port: 5173,
