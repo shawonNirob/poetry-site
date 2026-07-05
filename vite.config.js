c242918606998556715777868f71b6e1910c1d4e
shawonNirob <shawonnirob16@gmail.com>
2026-07-06 02:24:27 +0600

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/poetry-site/',
   server: {
     host: true,
     port: 5173,
